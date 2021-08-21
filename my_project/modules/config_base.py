e04eb9032a63a745ce6964c53287184d6b6de5f3
Nikita.Surkov <Nikita.Surkov@x5.ru>
2021-08-21 14:08:32 +0300

    Адаптация базовых классов под пример.

--- my_project/modules/config_base.py
+++ my_project/modules/config_base.py
@@ -1,6 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Задание класса конфига, адаптированного под конкретный проект
+"""
 import os
+from datetime import datetime
+import logging
+import sparkpip
 
 # Импорт конфигов
 MODULES_BASE_PATH = os.path.dirname(__file__)
-CFG_PATH = os.path.join(MODULES_BASE_PATH, '../my_project', 'config.yml')
-CFG_SOURCES = os.path.join(MODULES_BASE_PATH, '../my_project', 'config_sources.yml')
+CFG_PATH = os.path.join(MODULES_BASE_PATH, '..', 'config.yml')
+CFG_SOURCES = os.path.join(MODULES_BASE_PATH, '..', 'config_sources.yml')
+
+
+class ConfigBase(sparkpip.ConfigBasePattern):
+    """
+    Класс конфига, адаптированный под конкретный проект
+    """
+    def __init__(self):
+        super(ConfigBase, self).__init__(CFG_PATH, CFG_SOURCES, calc_date_format='%d.%m.%Y')
+        self.add_parameter('logging_level', '--logging_level', '-ll',
+                           type=int, required=False, const=10, default=10, nargs='?',
+                           help='10 - ERROR logging_level, 20 - info logging level')
+
+    def tune_logger(self, logger, date=None, log_prefix=None):
+        """
+        Инициализация параметров логгера
+        Parameters
+        ----------
+        logger : логгер
+        date : str, optional (default=today)
+         дата логов
+        log_prefix : str, optional (default=__name__)
+         префикс логов
+        """
+
+        if date is None:
+            date = datetime.today().date().strftime("%Y-%m-%d")
+
+        if log_prefix is None:
+            log_prefix = logger.name
+
+        log_file_name = 'log_{}_{}.log'.format(log_prefix, date)
+        log_file_path = os.path.join(MODULES_BASE_PATH, '..', 'data', 'logs', log_file_name)
+        logging.basicConfig(level=self.parameters['logging_level'],
+                            format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                            filename=log_file_path,
+                            filemode='w')
+        # define a Handler which writes INFO messages or higher to the sys.stderr
+        console = logging.StreamHandler()
+        console.setLevel(self.parameters['logging_level'])
+        # set a format which is simpler for console use
+        formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+        # tell the handler to use this format
+        console.setFormatter(formatter)
+        # add the handler to the root logger
+        logging.getLogger('').addHandler(console)
+
+        # Now, we can log to the root logger, or any other logger. First the root...
+        logging.info('initial log row')
+        # отключим массивные уведомления от spark
+        logging.getLogger("py4j").setLevel(logging.ERROR)
+        # отключим массивные уведомления от matplotlib
+        logging.getLogger("matplotlib").setLevel(logging.ERROR)
---
+++ my_project/modules/module_base.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Задание классов шагов и пайплайнов, адаптированноых под конкретный проект
+
+В данном примере ничего нового в классы не добавлялось
+"""
+import abc
+from typing import Dict
+
+import sparkpip
+
+
+class StepBase(sparkpip.StepBasePattern):
+    """
+    Класс типа ШАГ, адаптированный под конкретный проект
+    """
+    def __init__(self, spark, config, argument_tables=None, test=False, logger=None):
+        """
+        Parameters
+        ----------
+        spark : запущенный спарк
+        config : обект типа ConfigBase
+            конфиги проекта
+        argument_tables : dict, optional (default=None)
+            таблицы, передаваемые в качестве аргументов. Должны соответствовать таблицам из cls.source_table,
+            имеющим 'link' == 'argument'
+        test : bool
+            если модуль запускается в тестировании, то все self.source_tables понимаются как 'argument'
+        logger : logger, optional (default=None)
+            При None инициализируется свой логгер
+        """
+        super(StepBase, self).__init__(spark, config, argument_tables, test, logger)
+
+    @abc.abstractmethod
+    def _calculations(self) -> Dict:
+        """
+        Вычисление итоговых таблиц, указанных в cls.output_tables
+
+        Данный метод используется в методе класса run()
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        tables : dict
+            словарь вычисленных таблиц
+        """
+
+
+class PipelineBase(sparkpip.PipelineBasePattern):
+    """
+    Класс типа ШАГ, адаптированный под конкретный проект
+    """
+
+    def __init__(self, spark, config,
+                 step_sequence=None,
+                 logger=None,
+                 test_arguments=None,
+                 skip_structure_check=False,
+                 fix_nulls=True
+                 ):
+        """
+        Parameters
+        ----------
+        spark : запущенный спарк
+        config : ConfigBase
+            конфиги проекта
+        step_sequence : List[StepBase], optional (default=cls.step_sequence)
+            список шагов пайплайна
+        logger : logger, optional (default=None)
+            При None инициализируется свой логгер
+        test_arguments: dict, optional (default={})
+            словарь таблиц аргументов шагов для тестирования. Передавать только для тестирования!
+        skip_structure_check: bool, optional (default=False)
+            При тестах может появиться необходимость проверить работу частичного пайплана
+            (который будет начинаться с шагов, принимающих таблицы в качестве аргументов).
+            Такой пайплайн не пройдёт проверку на структуру. Чтобы её пропустить, задать параметр ``True``.
+        fix_nulls: bool, optional (default=True)
+            Чинить ли пустые значения выходных таблиц в соответствии с ulits.convert_to_nulls
+        """
+        super(PipelineBase, self).__init__(spark, config, step_sequence, logger,
+                                           test_arguments, skip_structure_check, fix_nulls)
